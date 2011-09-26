ac103fa07229f01a5b4ac124e4a0271568e72c3b
stephenjw <stephenjw@9e6f6ca4-5096-ecd5-4cac-d248a61cfab0>
2011-09-26 04:01:17 +0000

    Initial import

---
+++ Flattener/src/com/iteran/flattener/Main.java
@@ -0,0 +1,38 @@
+/* 
+ * Folder Flattener.
+ * (c)2011 Stephen J. Sarma-Weierman
+ * www.iteran.com
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package com.iteran.flattener;
+
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author sjw
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (Exception e) {
+      //I don't care if this happens. :-P
+    }
+        FlattenerGUI gui = new FlattenerGUI();
+        gui.setVisible(true);
+    }
+}
